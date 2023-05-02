9add4b9961608d00073539b0ff0c4bc2692ae01c
Marek Szulc <235991@edu.p.lodz.pl>
2023-05-02 17:37:05 +0200

    Add setting of default spinner values in the settings

--- app/src/main/java/com/weather/weatherapp/SettingsFragment.java
+++ app/src/main/java/com/weather/weatherapp/SettingsFragment.java
@@ -1,12 +1,25 @@
 package com.weather.weatherapp;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
+
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class SettingsFragment extends Fragment {
+
+    private Spinner unitsSpinner;
+    private Spinner frequencySpinner;
+    private SettingsParser settingsParser;
+
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -19,12 +32,43 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try {
+            settingsParser = new SettingsParser(requireContext());
+        } catch (ConfigurationException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view =  inflater.inflate(R.layout.fragment_settings, container, false);
+
+        setDefaultSpinnerValues(view);
+
+        return view;
+    }
+
+    private void setDefaultSpinnerValues(View view) {
+        unitsSpinner = view.findViewById(R.id.unitsSpinner);
+        frequencySpinner = view.findViewById(R.id.frequencySpinner);
+
+        try {
+            String units = settingsParser.getUnits();
+            int frequency = settingsParser.getFrequency();
+
+            Resources res = getResources();
+            String[] unitsArray = res.getStringArray(R.array.units_array);
+            String[] frequencyArray = res.getStringArray(R.array.frequency_array);
+
+            int unitsPosition = Arrays.asList(unitsArray).indexOf(units);
+            int frequencyPosition = Arrays.asList(frequencyArray).indexOf(String.valueOf(frequency));
+
+            unitsSpinner.setSelection(unitsPosition);
+            frequencySpinner.setSelection(frequencyPosition);
+        } catch (ConfigurationException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
